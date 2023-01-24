12fdd48bc6fc460a1986fd40da9abd571b16241e
Marcelo <marcelo3x4@hotmail.com>
2023-01-24 18:10:17 -0300

    financial details finish

--- src/pages/FinancialDetails/index.jsx
+++ src/pages/FinancialDetails/index.jsx
@@ -1,9 +1,35 @@
-import { Container } from "./styles";
+import {AiOutlineArrowUp} from 'react-icons/ai'
+import { Container, Content} from "./styles";
 import { Header } from "../../components/header";
+import {Button} from '../../components/button'
 export function FinancialDetails () {
     return(
         <Container>
             <Header />
+            <main>
+                <Content>
+                    <h1>Investimento</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Iusto maxime architecto fugit alias maiores obcaecati 
+                        tempora similique, iste, eius enim dolores 
+                        atque dicta quas molestias, voluptatum nisi quos temporibus ea.
+                    </p>
+                    
+                    
+                    <div>
+                        Entrada
+                        <span>
+                            <AiOutlineArrowUp />
+                        </span>
+                    </div>
+                    
+                    <p>
+                        Valor: 707,35 R$
+                    </p>    
+                    <Button title="Voltar" />
+                </Content>
+            </main>
         </Container>
     )
 }
--- src/pages/FinancialDetails/styles.js
+++ src/pages/FinancialDetails/styles.js
@@ -9,4 +9,51 @@ export const Container = styled.div`
         "header"
         "content"
     ;
+    
+    > main {
+        grid-area: content;
+        overflow-y: auto;
+        padding: 64px 0;
+    }
+
+`;
+
+export const Content = styled.div`
+    border-radius: 10PX;
+    max-width: 450px;
+
+    margin: 0 auto;
+    padding: 5px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+
+    > div {
+        background: ${({theme}) => theme.COLORS.BACKGROUND_400};
+        border-radius: 10px;
+        padding:12px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        > span {
+            color: ${({theme}) => theme.COLORS.GREEN_300};
+        }
+    }
+
+    > p {
+        text-align: justify;
+    }
+
+    > p:nth-child(4) {
+        background: ${({theme}) => theme.COLORS.BACKGROUND_400};
+        border-radius: 10px;
+        padding:12px;
+    }
+
+    > button {
+        align-self: center;
+        width: 100%;
+    }
+
 `;
