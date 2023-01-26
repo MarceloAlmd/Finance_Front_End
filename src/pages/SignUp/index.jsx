c65026e30410a090108d5313812b4736779b8e09
Marcelo <marcelo3x4@hotmail.com>
2023-01-25 21:27:44 -0300

    user registration created

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
-import {HiOutlineMail, HiOutlineLockClosed, HiOutlineUser} from 'react-icons/hi'
-import { Link } from 'react-router-dom';
-import {Input} from '../../components/input'
-import {Button} from '../../components/button'
+import {api} from '../../services/api';
+import {useState} from 'react';
+import {HiOutlineMail, HiOutlineLockClosed, HiOutlineUser} from 'react-icons/hi';
+import {Link, useNavigate} from 'react-router-dom';
+import {Input} from '../../components/input';
+import {Button} from '../../components/button';
 import { Container, Form} from "./styles";
 
 export function SignUp() {
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSignUp() {
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos");
+        };
+
+        api.post("/users", {name, email, password})
+        .then(() => {
+            alert("Usuário cadastrado com sucesso")
+            navigate("/");
+        })
+        .catch(error => {
+            if(error.response){
+                alert(error.response.data.message);
+            }else {
+                alert("não foi possível cadastrar");
+            };
+        });
+
+
+    };
     return (
         <Container>
             <Form>   
@@ -20,23 +46,29 @@ export function SignUp() {
                         icon={HiOutlineUser} 
                         placeholder="Nome"
                         type="text"
+                        onChange={e => setName(e.target.value)}
                     />
                     <Input 
                         icon={HiOutlineMail} 
                         placeholder="Email"
                         type="text"
+                        onChange={e => setEmail(e.target.value)}
                     />
 
                     <Input 
                         icon={HiOutlineLockClosed}
                         placeholder="Senha"
                         type="password"
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </div>
                 
                 
                 
-                <Button title="criar conta" />
+                <Button 
+                    title="criar conta" 
+                    onClick={handleSignUp}
+                />
 
                 <Link to="/">
                     voltar para o login
